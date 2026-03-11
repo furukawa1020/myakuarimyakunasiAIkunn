ba5760b8e6265c8dc6d4edc227e21fc4da2fed74
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-11 13:14:49 +0900

    feat: add initial Android platform build configuration and manifest files.

--- myakuari_ai/android/app/build.gradle.kts
+++ myakuari_ai/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myakuari_ai"
+    namespace = "com.furukawa.myakuari_ai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.myakuari_ai"
+        applicationId = "com.furukawa.myakuari_ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
