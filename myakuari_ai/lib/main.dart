b46a7edbdb00dfec164cbad2538e435e29f794ad
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-11 14:32:42 +0900

    feat: Initialize new Flutter application with main entry point and Android manifest.

--- myakuari_ai/lib/main.dart
+++ myakuari_ai/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '教えてAI君',
+      title: '教えてAI',
       theme: AppTheme.darkTheme,
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
