f941ce9c83abb8e6dddab889bbe5ac5756a2bf4c
furukawa1020 <f.kotaro.0530@gmail.com>
2026-03-11 17:42:37 +0900

    feat: Initialize Android application build configuration, including signing setup and project details.

--- myakuari_ai/android/app/build.gradle.kts
+++ myakuari_ai/android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.furukawa.myakuari_ai"
+    namespace = "com.furukawa.ai_diagnosis_kun"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.furukawa.myakuari_ai"
+        applicationId = "com.furukawa.ai_diagnosis_kun"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
